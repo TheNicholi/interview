d2739afe8260ca5e4d3711771984e43b81d6aa07
Nicholas Frisch <nicholasf@truckstop.com>
2022-03-02 10:57:24 -0700

    Updated the README.md to be more clear. Added a new task.

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ## Tasks
 
-1. Implement a new endpoint that will return the number of times each word appears in a given a list of words.
-2. Fix the `GetLongestWord` method in the statistics controller and make sure it can handle a null list.
+1. Fix the `GetLongestWord` method in the statistics controller and make sure it can handle an empty list.
+2. Implement a new endpoint that will return the number of times each word appears in a given list of words.
+3. Fix the unit test `Given_InvalidId_When_GettingVocabWord_Then_ThrowsException` so it doesn't pass when given valid IDs.
---
+++ Truckstop.Interview.Business/RealFakeDatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Truckstop.Interview.Business
+{
+    public static class RealFakeDatabaseContext
+    {
+        public static readonly ReadOnlyDictionary<int, string> VocabWords = new ReadOnlyDictionary<int, string>(new Dictionary<int, string> {
+            { 1, "Antecedent" },
+            { 2, "Average" },
+            { 3, "Bias" },
+            { 4, "Bimodal" },
+            { 5, "Complement" },
+            { 6, "Compound" },
+            { 7, "Event" },
+            { 8, "Factorial" },
+            { 9, "Frequency" },
+            { 10, "Graph" },
+            { 11, "Intersection" },
+            { 12, "Limit" },
+            { 13, "Margin" },
+            { 14, "Median" },
+            { 15, "Mode" }
+        });
+    }
+}
